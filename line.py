5ab1fbdc62593e9ecc86a25018548951acc14312
RedaMokarrab <reda.moukarrab@gmail.com>
2020-12-21 01:46:12 -0500

    First trial with search from previous

---
+++ line.py
@@ -0,0 +1,247 @@
+import matplotlib.pyplot as plt
+import cv2 
+import numpy as np
+
+
+
+#class lance to hold the lane global information 
+#it contains data for both lines + the curvature and off center information
+class Lane():
+    def __init__(self,previous_margin,nwindows,windowmargin,minipix):
+        self.left_line = self.Line()
+        self.right_line = self.Line()
+        # was the lane detected in the last iteration?
+        self.detected = False  
+        #lane midpoint in pixels
+        self.midpoint = None
+        #search from previous margin
+        self.margin = previous_margin
+        # Choose the number of sliding windows
+        self.nwindows = nwindows
+        # Set the width of the windows +/- margin
+        self.windowmargin = windowmargin
+        # Set minimum number of pixels found to recenter window
+        self.minpix = minipix
+        #radius of curvature of the lane meters (average of curve for both lines)
+        self.curve_m = None 
+    
+    # Define a line sub-class to receive the characteristics of each line detection
+    class Line():
+        def __init__(self):
+            #current x base for line 
+            self.xbase = None     
+            #polynomial coefficients for the most recent fit
+            self.current_fit = [np.array([False])] 
+            #xfit for most recent fit
+            self.current_fitx =None
+            #polynomial coefficients averaged over the last n iterations
+            self.best_fit = None  
+            #radius of curvature of the line in some units
+            self.radius_of_curvature = None 
+
+    #function will be called only in first frame of if the current frame was not able to fit lines
+    #search will start again
+    def find_lane_pixels_sliding_window(self,binary_warped):
+        # Take a histogram of the bottom half of the image
+        histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+        # Find the peak of the left and right halves of the histogram
+        # These will be the starting point for the left and right lines
+        midpoint = np.int(histogram.shape[0]//2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint  
+
+        # Set height of windows - based on nwindows above and image shape
+        window_height = np.int(binary_warped.shape[0]//self.nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated later for each window in nwindows
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+
+
+        # empty lists to receive new left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(self.nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_xleft_low = leftx_current - self.windowmargin
+            win_xleft_high = leftx_current + self.windowmargin
+            win_xright_low = rightx_current - self.windowmargin
+            win_xright_high = rightx_current + self.windowmargin
+        
+            # Identify the nonzero pixels in x and y within the window #
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+
+
+                # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > self.minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > self.minpix:        
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices (previously was a list of lists of pixels)
+        try:
+            left_lane_inds = np.concatenate(left_lane_inds)
+            right_lane_inds = np.concatenate(right_lane_inds)
+        except ValueError:
+            # Avoids an error if the above is not implemented fully
+            pass
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+        
+        #update class attributes
+        #save lane mid point for off center calculation
+        self.midpoint=midpoint
+        self.left_line.xbase = leftx_base
+        self.right_line.xbase =rightx_base
+
+        
+        return leftx, lefty, rightx, righty,
+
+
+    def find_lane_pixels_search_previous(self,binary_warped):
+        
+       
+        #grab search margin outside the previous fit
+        margin = self.margin
+        #grab the polyfit from last frame 
+        left_fit= self.left_line.current_fit
+        right_fit=self.right_line.current_fit
+        
+        # Grab activated pixels
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+
+        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
+                    left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
+                    left_fit[1]*nonzeroy + left_fit[2] + margin)))
+        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
+                    right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
+                    right_fit[1]*nonzeroy + right_fit[2] + margin)))
+    
+        # Again, extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+        
+        
+        return leftx, lefty, rightx, righty 
+    
+    def fit_polynomial(self,binary_warped):
+        #if first frame or last frame didn't detect a lane start searching from begining otherwise
+        #search from within last frame data 
+        if(self.detected == False):        
+            # Find our lane pixels first
+            leftx, lefty, rightx, righty = self.find_lane_pixels_sliding_window(binary_warped)
+            #print("find_lane_pixels_sliding_window")
+ 
+        else:
+            #print("find_lane_pixels_search_previous")
+            leftx, lefty, rightx, righty = self.find_lane_pixels_search_previous(binary_warped)
+       
+        
+        # Fit a second order polynomial to each using `np.polyfit`
+        left_fit = np.polyfit(lefty, leftx, 2)   #retunrs A,B and C coffiecents
+        right_fit = np.polyfit(righty, rightx, 2) #retunrs A,B and C coffiecents 
+        
+        return left_fit,right_fit
+    
+    
+    
+    def measure_curvature_pixels(self,left_fit,right_fit,y_eval):
+        
+    
+        # Calculation of R_curve (radius of curvature)
+        left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+        right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+        
+        return left_curverad,right_curverad
+
+    
+    def lane_fit_sanity_check(self,left_curve,right_curve):
+        status=True
+        
+        if((self.left_line.radius_of_curvature!=None) & (self.right_line.radius_of_curvature!=None)):
+            #check line curve is almost the same (30 degree difference is ok)
+            if(abs(left_curve-self.left_line.radius_of_curvature)>0.5):
+                status=False
+            if(abs(right_curve-self.right_line.radius_of_curvature)>0.5):
+                status=False
+
+        
+        return status
+    
+    def get_lane_highlighted(self,binary_warped):
+      
+
+        left_fit,right_fit =self.fit_polynomial(binary_warped)
+        
+        left_curve,right_curve = self.measure_curvature_pixels(left_fit,right_fit,binary_warped.shape[0])
+        
+        
+        #check if new fit makes sense 
+        if(self.lane_fit_sanity_check(left_curve,right_curve)):
+            #save current line fit
+            self.left_line.current_fit   =left_fit
+            self.right_line.current_fit  =right_fit
+            #save current curvature
+            self.left_line.radius_of_curvature = left_curve
+            self.right_line.radius_of_curvature = right_curve
+        else:
+            #reset to use the sliding window method and use old values 
+            left_fit= self.left_line.current_fit   
+            right_fit= self.right_line.current_fit
+            left_curve=self.left_line.radius_of_curvature 
+            right_curve = self.right_line.radius_of_curvature
+            self.detected = False
+           
+        
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        try:
+            left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+            right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+            #save fit x 
+            self.left_line.current_fitx =left_fitx
+            self.right_line.current_fitx = right_fitx
+            
+            self.detected = True  #mark lane as detected
+        except TypeError:
+            # use last frame lines 
+            print('The function failed to fit a line!')
+            left_fitx = self.left_line.current_fitx
+            right_fitx  = self.right_line.current_fitx
+            self.detected = False #mark lane as not detected and start search from next frame and keep old data from previous n frames
+        
+        
+        lane_highlight_left=  np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        lane_highlight_right=  np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        lane_highlight_pts = np.hstack((lane_highlight_left, lane_highlight_right))
+        
+       
+        
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))
+        window_img = np.zeros_like(out_img)
+        cv2.fillPoly(window_img,np.int_([lane_highlight_pts]),(0,255,0))
+     
+
+        return window_img
---
+++ support_functions.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import cv2 
+import numpy as np
+
+
+def x_gradient(channel, threshould ):
+
+    # Sobel x
+    sobelx = cv2.Sobel(channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8((255*abs_sobelx)/(np.max(abs_sobelx)))
+    # Threshold x gradient
+    xbinary = np.zeros_like(scaled_sobel)
+    xbinary[(scaled_sobel >= threshould[0]) & (scaled_sobel <= threshould[1])] = 1
+
+    return xbinary
+
+#function takes undistorted image and runs x gradient for v channel and l channel
+#also runs color channel filter on s channel 
+def lane_filter(undist_img, s_thresh,l_thresh,x_thresh):
+
+    #Convert to HSV
+    hsv = cv2.cvtColor(undist_img, cv2.COLOR_RGB2HSV)
+    #convert to HLS 
+    hls = cv2.cvtColor(undist_img, cv2.COLOR_RGB2HLS)
+
+    v_channel = hsv[:,:,2]
+    s_channel = hsv[:,:,1]
+    l_channel = hls[:,:,1]
+
+    #get gradient for both v and l channels
+    vxbinary= x_gradient(v_channel,x_thresh)
+    lxbinary= x_gradient(l_channel,x_thresh)
+
+    # Threshold color channel s 
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
+    # Threshold color channel l 
+    l_binary = np.zeros_like(l_channel)
+    l_binary [((l_channel>= l_thresh[0])& (l_channel<= l_thresh[1]))]=1
+
+
+    combined_binary = np.zeros_like(lxbinary)
+    combined_binary[(vxbinary == 1)|(lxbinary==1)|(s_binary==1)|(l_binary==1)] = 1
+
+
+    return combined_binary
+
+
+
